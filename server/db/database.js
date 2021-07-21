b5623c0ee5fa3bb1ada4fbeef1ee130421b60634
Vlad C <vladis828@gmail.com>
2021-07-21 11:02:50 -0400

    database properties changed

--- server/db/database.js
+++ server/db/database.js
@@ -7,8 +7,13 @@ const Sequelize = require('sequelize');
  */
 
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/blog_store', {
-  logging: false // unless you like the logs
+  logging: false, // unless you like the logs
   // ...and there are many other options you may want to play with
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 module.exports = db;
