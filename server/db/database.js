40642b0ff69c73a33d909170c499a5aed2a98590
Vlad C <vladis828@gmail.com>
2021-07-21 14:03:00 -0400

    database.js db definition separated

--- server/db/database.js
+++ server/db/database.js
@@ -5,18 +5,24 @@ const Sequelize = require('sequelize');
 /**
  * !!!! Change 'yourdbname' to the actual db name !!!!
  */
-
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/blog_store', {
-  logging: false, // unless you like the logs
-  // ...and there are many other options you may want to play with
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+let db;
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    logging: false, // unless you like the logs
+    // ...and there are many other options you may want to play with
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU NEED THIS TO AVOID SequelizeConnectionError: self signed certificate
+      }
     }
-  }
-});
+  })
+} else {
+  db = new Sequelize('postgres://localhost:5432/blog_store', {
+    logging: false
+  })
+};
 
 module.exports = db;
